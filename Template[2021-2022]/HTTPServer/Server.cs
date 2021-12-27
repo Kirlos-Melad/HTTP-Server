3fe402f7f5ec5f262e2ac077c9dc57713ff8eb77
Alyaa <66500059+alyaamansor20@users.noreply.github.com>
2021-12-27 16:26:50 +0200

    Update Server.cs

--- Template[2021-2022]/HTTPServer/Server.cs
+++ Template[2021-2022]/HTTPServer/Server.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,49 +16,70 @@ namespace HTTPServer
         public Server(int portNumber, string redirectionMatrixPath)
         {
             //TODO: call this.LoadRedirectionRules passing redirectionMatrixPath to it
+            this.LoadRedirectionRules(redirectionMatrixPath);
+
             //TODO: initialize this.serverSocket
+            this.serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
 
         public void StartServer()
         {
             // TODO: Listen to connections, with large backlog.
+            this.serverSocket.Listen(200);
 
             // TODO: Accept connections in while loop and start a thread for each connection on function "Handle Connection"
             while (true)
             {
                 //TODO: accept connections and start thread for each accepted connection.
-
+                Socket clientSocket = this.serverSocket.Accept();
+                Thread newThread = new Thread(new ParameterizedThreadStart(HandleConnection));
+                ThreadPool.QueueUserWorkItem(HandleConnection, clientSocket);
+                newThread.Start(clientSocket);
             }
         }
 
         public void HandleConnection(object obj)
         {
             // TODO: Create client socket 
+            Socket _clientSocket = (Socket)obj;
             // set client socket ReceiveTimeout = 0 to indicate an infinite time-out period
-            
+            _clientSocket.ReceiveTimeout = 0;
             // TODO: receive requests in while true until remote client closes the socket.
             while (true)
             {
                 try
                 {
                     // TODO: Receive request
+                    byte[] data = new byte[1024];
+                    int receivedLength = _clientSocket.Receive(data);
 
                     // TODO: break the while loop if receivedLen==0
+                    if (receivedLength == 0)
+                        break;
 
                     // TODO: Create a Request object using received request string
+                    string received = Encoding.ASCII.GetString(data, 0, receivedLength);
+                    Request request_ = new Request(received);
 
                     // TODO: Call HandleRequest Method that returns the response
+                    Response _response;
+                    _response = HandleRequest(request_);
+                    string response = _response.ToString();
 
                     // TODO: Send Response back to client
+                    data = Encoding.ASCII.GetBytes(response);
+                    _clientSocket.Send(data);
 
                 }
                 catch (Exception ex)
                 {
                     // TODO: log exception using Logger class
+                    Logger.LogException(ex);
                 }
             }
 
             // TODO: close client socket
+            _clientSocket.Close();
         }
 
         Response HandleRequest(Request request)
@@ -89,7 +110,12 @@ namespace HTTPServer
         private string GetRedirectionPagePathIFExist(string relativePath)
         {
             // using Configuration.RedirectionRules return the redirected page path if exists else returns empty
-            
+            foreach (KeyValuePair<string, string> kvp in Configuration.RedirectionRules) { 
+                if (relativePath == kvp.Value)
+                {
+                    return kvp.Value;
+                }
+            }
             return string.Empty;
         }
 
@@ -97,7 +123,7 @@ namespace HTTPServer
         {
             string filePath = Path.Combine(Configuration.RootPath, defaultPageName);
             // TODO: check if filepath not exist log exception using Logger class and return empty string
-            
+
             // else read file and return its content
             return string.Empty;
         }
