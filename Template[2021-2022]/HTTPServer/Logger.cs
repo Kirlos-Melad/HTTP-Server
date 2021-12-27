7301e0734c7a23cee4638c555925a9bfa8eee1c7
marihanhany <86060417+marihanhany@users.noreply.github.com>
2021-12-27 19:48:08 +0200

    logger & program
    logger & program

--- Template[2021-2022]/HTTPServer/Logger.cs
+++ Template[2021-2022]/HTTPServer/Logger.cs
@@ -8,13 +8,23 @@ namespace HTTPServer
 {
     class Logger
     {
-        static StreamWriter sr = new StreamWriter("log.txt");
         public static void LogException(Exception ex)
         {
             // TODO: Create log file named log.txt to log exception details in it
             //Datetime:
             //message:
             // for each exception write its details associated with datetime 
+            if (!File.Exists("log.txt"))
+            {
+                FileStream fs = new FileStream("log.txt", FileMode.CreateNew);
+                fs.Close();
+            }
+            using (StreamWriter sw = File.AppendText("log.txt"))
+            {
+                sw.WriteLine("Datetime: " + ex.Message);
+                sw.WriteLine("message: " + DateTime.Now.ToString());
+                sw.Close();
+            }
         }
     }
 }
--- Template[2021-2022]/HTTPServer/Program.cs
+++ Template[2021-2022]/HTTPServer/Program.cs
@@ -11,14 +11,44 @@ namespace HTTPServer
         static void Main(string[] args)
         {
             // TODO: Call CreateRedirectionRulesFile() function to create the rules of redirection 
-            
+            CreateRedirectionRulesFile();
             //Start server
             // 1) Make server object on port 1000
             // 2) Start Server
+            Server server = new Server(1000, "redirectionRules.txt");
+            Console.WriteLine("started....");
+            server.StartServer();
         }
 
         static void CreateRedirectionRulesFile()
         {
+            string oldURI = " ";
+            string newURI = " ";
+            // TODO: Create file named redirectionRules.txt
+            if (!File.Exists("redirectionRules.txt"))
+            {
+                FileStream fs = new FileStream("redirectionRules.txt", FileMode.CreateNew);
+                fs.Close();
+            }
+
+            StreamWriter sw = new StreamWriter("redirectionRules.txt");
+            sw.WriteLine("aboutus.html,aboutus2.html");
+
+
+            //StreamReader sr = new StreamReader("redirectionRules.txt");
+            string[] lines = File.ReadAllLines("redirectionRules.txt");
+            foreach (string line in lines)
+            {
+                string[] splitLine = line.Split(',');
+                oldURI = splitLine[0].ToString();
+                newURI = splitLine[1].ToString();
+
+            };
+
+            if (Configuration.RedirectionRules.ContainsKey(oldURI))
+            {
+                Response res = new Response(StatusCode.Redirect, "text/html", "redirect.html", Configuration.RedirectionRules[oldURI]);
+            }
             // TODO: Create file named redirectionRules.txt
             // each line in the file specify a redirection rule
             // example: "aboutus.html,aboutus2.html"
